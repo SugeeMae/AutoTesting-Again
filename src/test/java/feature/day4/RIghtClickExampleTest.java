70d207e658f23a780faadd4a7f93d78d7e9f11a0
SugeeMae <sunnymae2808@gmail.com>
2024-12-16 12:14:40 +0700

    Nothing changed

--- src/test/java/feature/day4/RIghtClickExampleTest.java
+++ src/test/java/feature/day4/RIghtClickExampleTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+
 public class RIghtClickExampleTest {
     public static void main(String[] args) {
         // Khởi tạo WebDriver
--- src/test/java/feature/day4/SingleRadioButtonSelectionTest.java
+++ src/test/java/feature/day4/SingleRadioButtonSelectionTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class SingleRadioButtonSelectionTest {
     public static void main(String[] args) {
         // Khởi tạo WebDriver
